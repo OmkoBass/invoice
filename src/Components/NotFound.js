30436c1c010970dcd5304a1752b049b4cd6b9b53
OmkoBass <omkob@hotmail.com>
2020-05-12 00:37:58 +0200

    Beautiful 404 page.

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
-function NotFound() {
-    return <div>
+//antd
+import {Row, Col, Layout, Result} from 'antd'
 
-    </div>
+//not found image
+import notFound from '../Assets/404.png'
+
+//skyon
+import skyon from '../Assets/skyonlight.png'
+
+const { Header, Content } = Layout
+
+function NotFound() {
+    return <Layout>
+        <Header>
+            <img src={skyon} alt='skyon logo' style={{width:'100px'}}/>
+        </Header>
+        <Content style={{backgroundColor: 'white'}}>
+            <Result
+                status='error'
+                title='Stranica ne postoji!'
+                subTitle='Stranica kojoj želite da pristupite ne postoji.'
+                extra={[
+                    <Row justify='center'>
+                        <Col xl={12} lg={18} xs={24}>
+                            <img style={{width: '100%'}} src={notFound} alt='not found'/>
+                        </Col>
+                    </Row>
+                ]}
+            />
+        </Content>
+    </Layout>
 }
 
 export default NotFound;
