cee090722d7d57317557ac6d63b8b9e8133cdaac
OmkoBass <omkob@hotmail.com>
2020-05-12 19:11:28 +0200

    Beautiful Login page.

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -4,11 +4,13 @@ import React from 'react'
 import {useHistory} from "react-router";
 
 //antd
-import { Form, Input, Button, message} from 'antd';
+import { Form, Input, Button, Layout, message} from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 //skyon
 import skyon from '../Assets/skyondark.png'
+
+const { Content } = Layout;
 
 //antd password field
 const {Password} = Input;
@@ -32,41 +34,45 @@ function LoginPage() {
 
     let history = useHistory();
 
-    return <div className='login'>
-        <img src={skyon} alt='skyon logo' style={{width: '100%', height: '25%'}}/>
-        <Form
-            name="login"
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-        >
-            <Form.Item
-                name="username"
-                rules={[{ required: true, message: 'Unesite korisničko ime!' }]}
-            >
-                <Input prefix={<UserOutlined/>} placeholder="Korisničko ime" />
-            </Form.Item>
-            <Form.Item
-                name="password"
-                rules={[{ required: true, message: 'Unesite lozinku!' }]}
-            >
-                <Password
-                    prefix={<LockOutlined/>}
-                    type="password"
-                    placeholder="Lozinka"
-                />
-            </Form.Item>
+    return <Layout>
+        <Content style={{height: '100vh'}}>
+            <div className='login'>
+                <img src={skyon} alt='skyon logo' style={{width: '100%', height: '25%'}}/>
+                <Form
+                    name="login"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Unesite korisničko ime!' }]}
+                    >
+                        <Input prefix={<UserOutlined/>} placeholder="Korisničko ime" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Unesite lozinku!' }]}
+                    >
+                        <Password
+                            prefix={<LockOutlined/>}
+                            type="password"
+                            placeholder="Lozinka"
+                        />
+                    </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Prijavite se
-                </Button>
-                Ili
-                <Button type='default'>
-                    Registrujte se!
-                </Button>
-            </Form.Item>
-        </Form>
-    </div>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Prijavite se
+                        </Button>
+                        Ili
+                        <Button type='default'>
+                            Registrujte se!
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </Content>
+    </Layout>
 }
 
 export default LoginPage;
