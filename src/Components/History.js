3d0a80eea00a2c1a604aa5e7beffbd718240d99c
OmkoBass <omkob@hotmail.com>
2020-08-01 14:44:45 +0200

    Pulling data now works.

--- src/Components/History.js
+++ src/Components/History.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+
+//Firebase
+import firebase from "../firebase";
+
+//Ant components
+import {Table} from "antd";
+
+//Context
+import {AuthContext} from "./Auth";
+import ErrorResult from "./Smaller/ErrorResult";
 
 function History() {
-    return <div>
+    const {currentUser} = useContext(AuthContext);
 
+    const [invoices, setInvoices] = useState([]);
+    const [load, setLoad] = useState(true);
+    const [error, setError] = useState(false);
+
+    const columns = [
+        {
+            title: 'Faktura',
+            dataIndex: 'invoice'
+        },
+        {
+            title: 'Kome',
+            dataIndex: 'toName'
+        },
+        {
+            title: 'Adresa',
+            dataIndex: 'toAddress'
+        },
+        {
+            title: 'Datum izdavanja',
+            dataIndex: 'dateCreated'
+        },
+    ];
+
+    useEffect(() => {
+        firebase.database().ref(`users/${currentUser.uid}/invoices`).once('value')
+            .then(data => {
+                setInvoices(Object.values(data.val()));
+            })
+            .then(() => setLoad(false))
+            .catch(() => setError(true));
+    }, [currentUser.uid]);
+
+    return <div>
+        {
+            error
+                ?
+                <ErrorResult/>
+                :
+                <Table
+                    bordered
+                    loading={load}
+                    columns={columns}
+                    dataSource={invoices.map((invoice, index) => {
+                    return {
+                        key: index,
+                        invoice: invoice.invoice,
+                        toName: invoice.toName,
+                        toAddress: invoice.toAddress,
+                        dateCreated: invoice.dateCreated
+                    }
+                })}/>
+        }
     </div>
 }
 
