2a03a5f2a55fa6754a7bc0c4dba3bccf47d12ed5
OmkoBass <omkob@hotmail.com>
2020-08-12 19:56:58 +0200

    Invoices can now be pulled

--- src/Components/History.js
+++ src/Components/History.js
@@ -1,16 +1,24 @@
-import React, {useState, useEffect, useContext, useRef} from 'react'
+import React, { useState, useEffect, useContext, useRef } from 'react'
 
 //Ant components
-import {Table, Row, Col, Button, notification, Typography, Divider, Input} from "antd";
+import { Table, Row, Col, Button, notification, Typography, Divider, Input } from "antd";
+
+import axios from 'axios';
 
 //Components
 import PDF from "./PDF";
 import ErrorResult from "./Smaller/ErrorResult";
 
 //Debounce
-import {debounce} from 'lodash';
+import { debounce } from 'lodash';
 
-function History() {
+import DATABASE from "../Utils";
+
+import { AuthContext } from "./Auth";
+
+function History () {
+    const { currentUser } = useContext(AuthContext);
+
     const [invoices, setInvoices] = useState(null);
     const [pdfData, setPdfData] = useState(null);
     const [load, setLoad] = useState(true);
@@ -29,7 +37,7 @@ function History() {
     const columns = [
         {
             title: 'ID fakture',
-            dataIndex: 'id'
+            dataIndex: '_id'
         },
         {
             title: 'Faktura',
@@ -50,8 +58,18 @@ function History() {
     ];
 
     useEffect(() => {
-
-    }, [defaultPageSize, pageNumber]);
+        axios.post(`${ DATABASE }/get/invoices`, {
+            username: currentUser.username
+        }).then(res => {
+            if (res.data === 400) {
+                setInvoices(null);
+            } else {
+                setInvoices(res.data);
+            }
+        }).then(() => setLoad(false)).catch(() => {
+            setError(true);
+        })
+    }, [currentUser.username]);
 
     const rowSelection = {
         selectedRowKeys,
@@ -61,27 +79,13 @@ function History() {
         }
     };
 
-    /*const rowSelection = {
-        onChange: (selectedRowKeys, selectedRows) => {
-            setSelected(selectedRows);
-            setSelectedRowKeys(selectedRowKeys);
-        }
-    };*/
-
-    const failNotification = () => {
-        notification.error({
-            message: 'Greška!',
-            description: 'Došlo je do greške pri čuvanju fakture.'
-        });
-    }
-
     const deleteNotification = () => {
         notification.success({
             message: 'Uspešno!',
         })
     }
 
-    return <div style={{minHeight: '80vh'}}>
+    return <div style={ { minHeight: '80vh' } }>
         <Typography>
             <Typography.Title>Istorija</Typography.Title>
             <Typography.Paragraph> Ovde mozete videti istoriju vaših faktura. </Typography.Paragraph>
@@ -97,60 +101,59 @@ function History() {
                         <Input
                             placeholder='Pretražite fakturu'
                             allowClear
-                            ref={searchRef}
-                            style={{marginBottom: '1em'}}
-                            onChange={debounce(() => {
+                            ref={ searchRef }
+                            style={ { marginBottom: '1em' } }
+                            onChange={ debounce(() => {
                                 console.log(searchRef.current.state.value);
-                            }, 500)}
+                            }, 500) }
                         />
                         <Table
                             bordered
-                            onChange={pagination => {
-                                if((pagination.current * pagination.defaultPageSize) >= defaultPageSize * pageNumber)
+                            onChange={ pagination => {
+                                if ((pagination.current * pagination.defaultPageSize) >= defaultPageSize * pageNumber)
                                     setPageNumber(pageNumber + 1);
-                            }}
-                            pagination={{defaultPageSize: defaultPageSize - 1}}
-                            rowSelection={rowSelection}
-                            loading={load}
-                            columns={columns}
+                            } }
+                            pagination={ { defaultPageSize: defaultPageSize - 1 } }
+                            rowSelection={ rowSelection }
+                            loading={ load }
+                            columns={ columns }
                             /*onRow={(record) => {
                                 return {
                                     onClick: _ => setPdfData(record)
                                 }
                             }}*/
-                            dataSource={invoices?.map((invoice, index) => {
+                            dataSource={ invoices?.map((invoice, index) => {
                                 return {
-                                    ...invoice[1],
+                                    ...invoice,
                                     key: index,
-                                    id: invoice[0]
                                 }
-                            })}/>
+                            }) }/>
 
-                        <Row gutter={12} style={{margin: '1em 0 1em 0'}}>
+                        <Row gutter={ 12 } style={ { margin: '1em 0 1em 0' } }>
                             <Col>
                                 <Button
-                                    onClick={() => {
+                                    onClick={ () => {
                                         setPdfData(selected[0]);
                                         setSelectedRowKeys([]);
-                                    }}
+                                    } }
                                     type='primary'
-                                    disabled={selectedRowKeys?.length !== 1}
+                                    disabled={ selectedRowKeys?.length !== 1 }
                                 >
                                     Prikaži
                                 </Button>
                             </Col>
                             <Col>
                                 <Button
-                                    onClick={() => {
+                                    onClick={ () => {
                                         setSelectedRowKeys([]);
                                         //Delete from firebase
 
                                         //Updating state
                                         setInvoices(invoices.filter(invoice => selected.find(select => select.id !== invoice[0])));
-                                    }}
+                                    } }
                                     type='primary'
                                     danger
-                                    disabled={!selectedRowKeys?.length > 0}
+                                    disabled={ !selectedRowKeys?.length > 0 }
                                 >
                                     Izbriši
                                 </Button>
@@ -163,7 +166,7 @@ function History() {
         {
             pdfData
                 ?
-                <PDF /*image={img}*/ info={pdfData} style={{height: '100vh', width: '100%'}}/>
+                <PDF /*image={img}*/ info={ pdfData } style={ { height: '100vh', width: '100%' } }/>
                 :
                 null
         }
