46138759f36801f378a04e01666cc3a46b231cd9
OmkoBass <omkob@hotmail.com>
2020-08-12 14:14:17 +0200

    Basic login and register works.

--- Backend/mongoose.js
+++ Backend/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
 const User = require('./Models/User');
 
 mongoose.connect('mongodb://127.0.0.1:27017/Invoice')
@@ -10,18 +11,51 @@ mongoose.connect('mongodb://127.0.0.1:27017/Invoice')
 });
 
 const createUser = async (req, res) => {
-    const createdUser = new User({
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password,
-    });
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-    createdUser.save((err, result) => {
-       if(err)
-           res.json(400);
-       else
-           res.json(createdUser);
-    });
+        const createdUser = new User({
+            email: req.body.email,
+            username: req.body.username,
+            password: hashedPassword,
+        });
+
+        createdUser.save((err, result) => {
+            if(err)
+                res.json(400);
+            else
+                res.json(result);
+        });
+    } catch {
+        await res.json(500);
+    }
+}
+
+const loginUser = async (req, res) => {
+    User.findOne({ username: req.body.username })
+        .lean().exec(async (err, result) => {
+            if(err)
+                await res.json(500);
+            else {
+                try {
+                    if(await bcrypt.compare(req.body.password, result.password)) {
+                        await res.json(result);
+                    } else {
+                        await res.json(401)
+                    }
+                } catch {
+                    await res.json(400);
+                }
+            }
+    })
+}
+
+const getUsers = async (req, res) => {
+    const users = await User.find().exec();
+
+    await res.json(users);
 }
 
 exports.createUser = createUser;
+exports.loginUser = loginUser;
+exports.getUsers = getUsers;
--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
+const cors = require('cors');
 const mongoose = require('./mongoose');
 
 const app = express();
 
 // So i can send json
 app.use(express.json());
+app.use(cors());
 
 app.post('/create/user', mongoose.createUser);
+app.post('/login/user', mongoose.loginUser);
 
 app.listen(5000, () => {
     console.log('LISTENING AT PORT 5000');
