4a6bc3c026f144e59f8476739bbdb4115973f045
OmkoBass <omkob@hotmail.com>
2020-08-12 15:56:29 +0200

    Updated model.

--- Backend/Models/Invoice.js
+++ Backend/Models/Invoice.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
 
 const invoiceSchema = mongoose.Schema({
-    belongsTo: { type: String , ref: 'User' },
-    account: { type: String, max: 128 },
-    city: { type: String, max: 128 },
-    dateCreated: { type: String, max: 128 },
-    dateInvoice: { type: String, max: 128 },
-    dateTraffic: { type: String, max: 128 },
-    email: { type: String, max: 128 },
-    firmName: { type: String, max: 128 },
-    fromName: { type: String, max: 128 },
-    invoice: { type: String, max: 128 },
-    pib: { type: String, max: 128 },
-    place: { type: String, max: 128 },
-    street: { type: String, max: 128 },
-    toAddress: { type: String, max: 128 },
-    toCity: { type: String, max: 128 },
-    toName: { type: String, max: 128 },
-    toPib: { type: String, max: 128 },
+    belongsTo: { type: String , ref: 'User', required: true },
+    account: { type: String, max: 128, required: true },
+    city: { type: String, max: 128, required: true },
+    dateCreated: { type: String, max: 128, required: true },
+    dateInvoice: { type: String, max: 128, required: true },
+    dateTraffic: { type: String, max: 128, required: true },
+    email: { type: String, max: 128, required: true },
+    firmName: { type: String, max: 128, required: true },
+    fromName: { type: String, max: 128, required: true },
+    invoice: { type: String, max: 128, required: true },
+    pib: { type: String, max: 128, required: true },
+    place: { type: String, max: 128, required: true },
+    street: { type: String, max: 128, required: true },
+    toAddress: { type: String, max: 128, required: true },
+    toCity: { type: String, max: 128, required: true },
+    toName: { type: String, max: 128, required: true },
+    toPib: { type: String, max: 128, required: true },
     services: [{
-        amount: { type: String, max: 32 },
-        price: { type: String, max: 32 },
-        serviceType: { type: String, max: 32 },
-        total: { type: String, max: 32 },
-        unit: { type: String, max: 32 }
+        amount: { type: String, max: 32, required: true },
+        price: { type: String, max: 32, required: true },
+        serviceType: { type: String, max: 32, required: true },
+        total: { type: String, max: 32, required: true },
+        unit: { type: String, max: 32, required: true }
     }]
 });
 
