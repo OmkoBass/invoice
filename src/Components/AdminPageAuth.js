dd589eb35cc3b16f67a5a89aed55d36721b4a474
OmkoBass <omkob@hotmail.com>
2020-08-16 12:08:19 +0200

    You can log in on the admin page now

--- src/Components/AdminPageAuth.js
+++ src/Components/AdminPageAuth.js
@@ -4,57 +4,36 @@ import DATABASE from "../Utils";
 
 const AdminPageAuth = {
     login: ({ username, password }) =>  {
-        axios.post(`${DATABASE}/login/admin`, {
-           username: username,
-           password: password
+        return axios.post(`${DATABASE}/login/admin`, {
+            username: username,
+            password: password
         }).then(res => {
+            console.log(res);
             if(res.data === 401 || res.data === 400)
-                throw new Error('Check your credentials');
+                throw new Error('Check your credentials')
 
-            return res.data.json();
-        }).then(({ token }) => {
-            localStorage.setItem('User', token);
-
-            console.log(localStorage.getItem('User'));
-        })
-    },
-    /*login: ({ username, password }) =>  {
-        const request = new Request(`${DATABASE}/login/admin`, {
-            method: 'POST',
-            body: {
-                username: username,
-                password: password
-            },
-            headers: new Headers({ 'Content-Type': 'application/json' }),
+            localStorage.setItem('username', res.data);
+            return Promise.resolve();
         });
-        return fetch(request)
-            .then(response => {
-                if (response.status < 200 || response.status >= 300) {
-                    throw new Error('Check your credentials!');
-                }
-                return response.json();
-            })
-            .then(({ token }) => {
-                localStorage.setItem('User', token);
-            });
-    },*/
+    },
+    // called when the user clicks on the logout button
     logout: () => {
-        localStorage.removeItem('User');
+        localStorage.removeItem('username');
         return Promise.resolve();
     },
     // called when the API returns an error
     checkError: ({ status }) => {
         if (status === 401 || status === 403) {
-            localStorage.removeItem('User');
+            localStorage.removeItem('username');
             return Promise.reject();
         }
         return Promise.resolve();
     },
     // called when the user navigates to a new location, to check for authentication
     checkAuth: () => {
-        if(localStorage.getItem('User'))
-            return Promise.resolve();
-        return Promise.reject();
+        return localStorage.getItem('username')
+            ? Promise.resolve()
+            : Promise.reject();
     },
     // called when the user navigates to a new location, to check for permissions / roles
     getPermissions: () => Promise.resolve(),
