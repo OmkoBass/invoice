b7fe94254ac96a605222e701498cc9611cfc0b20
OmkoBass <omkob@hotmail.com>
2020-08-23 14:16:55 +0200

    Filters added

--- src/Components/Admin/User/UserList.js
+++ src/Components/Admin/User/UserList.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
-import {List, Datagrid, TextField, EmailField } from 'react-admin';
+import { List, Datagrid, TextField, EmailField, Filter, SearchInput } from 'react-admin';
 
-export default function UserList(props) {
-    return <List {...props}>
-        <Datagrid rowClick="edit" key={props.id}>
-            <TextField source="id" />
-            <TextField source="username" />
-            <EmailField source="email" />
-            <TextField source="password" />
+const UserFilter = (props) => (
+    <Filter { ...props }>
+        <SearchInput source='username' awlaysOn/>
+    </Filter>
+)
+
+export default function UserList (props) {
+    return <List filters={ <UserFilter/> } { ...props }>
+        <Datagrid rowClick="edit" key={ props.id }>
+            <TextField source="id"/>
+            <TextField source="username"/>
+            <EmailField source="email"/>
+            <TextField source="password"/>
         </Datagrid>
     </List>
 }
