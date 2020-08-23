121e990e21c55d312a0db4e264593b684df57014
OmkoBass <omkob@hotmail.com>
2020-08-23 14:25:59 +0200

    Updated

--- Backend/Models/Client.js
+++ Backend/Models/Client.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const clientSchema = mongoose.Schema({
+    belongsTo: { type: String , ref: 'User', required: true },
     toAddress: { type: String, max: 128, required: true },
     toCity: { type: String, max: 128, required: true },
     toName: { type: String, max: 128, required: true },
