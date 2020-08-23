c2134a660d1ddd24619761f125b3ecce4a7a94bf
OmkoBass <omkob@hotmail.com>
2020-08-23 13:04:03 +0200

    Updated editing

--- src/Components/Admin/User/UserEdit.js
+++ src/Components/Admin/User/UserEdit.js
@@ -1,21 +1,27 @@
 import React from 'react'
 
-import { Edit, SimpleForm, TextInput } from 'react-admin';
+import { Edit, email, required, SimpleForm, TextInput } from 'react-admin';
+
+const validateEmail = [required(), email()];
+
+const UserTitle = ({ record }) => {
+    return <span>User: { record ? record.username : '' }</span>
+}
 
 export const UserEdit = props => (
-    <Edit {...props}>
+    <Edit title={ <UserTitle/> } { ...props }>
         <SimpleForm>
-            <TextInput source="username" />
-            <TextInput source="email" />
-            <TextInput source="password" disabled />
-            <TextInput source="_id" disabled />
-            <TextInput label="From" source="profile.fromName" />
-            <TextInput label="Firm" source="profile.firmName" />
-            <TextInput label="Street" source="profile.street" />
-            <TextInput label="City" source="profile.city" />
-            <TextInput label="PIB" source="profile.pib" />
-            <TextInput label="Bank Account" source="profile.account" />
-            <TextInput label="Email" source="profile.email" />
+            <TextInput source="username" validate={ [required()] }/>
+            <TextInput source="email" validate={ validateEmail }/>
+            <TextInput source="password" disabled/>
+            <TextInput source="_id" disabled/>
+            <TextInput label="From" source="profile.fromName"/>
+            <TextInput label="Firm" source="profile.firmName"/>
+            <TextInput label="Street" source="profile.street"/>
+            <TextInput label="City" source="profile.city"/>
+            <TextInput label="PIB" source="profile.pib"/>
+            <TextInput label="Bank Account" source="profile.account"/>
+            <TextInput label="Email" source="profile.email"/>
         </SimpleForm>
     </Edit>
 );
